b8b44111f1f42f9ddfa5b03efde0120b515eecf1
Afnan Khalid <afnank19@gmail.com>
2024-03-25 22:26:10 +0500

    enhance card system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject levelUpOptions;
+    public LevelUpButtonMngr levelUpButtonMngr;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,7 @@ public class GameManager : MonoBehaviour
         //This is for testing the level up menu, in future this should be replaced by the Pause Menu
         //and other game management requirements
         if(Input.GetKeyDown(KeyCode.P)){
+            levelUpButtonMngr.setImages();
             levelUpOptions.SetActive(true);
             Time.timeScale = 0f;
         }
--- Assets/Scripts/LevelUpButtonMngr.cs
+++ Assets/Scripts/LevelUpButtonMngr.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -10,15 +11,19 @@ public class LevelUpButtonMngr : MonoBehaviour
     public BulletScript bullet;
     public GameObject PowerUpCanvas;
 
-    public Button option1;
-    Image option1Image;
+    public Button option1, option2, option3;
+    public Image option1Image, option2Image, option3Image;
     public Sprite[] testImage;
     // Start is called before the first frame update
     void Start()
     {
         bullet.force = 5.5f;
         option1Image = option1.GetComponent<Image>();
+        option2Image = option2.GetComponent<Image>();
+        option3Image = option3.GetComponent<Image>();
         option1Image.sprite = testImage[0];
+        option2Image.sprite = testImage[1];
+        option3Image.sprite = testImage[2];
     }
 
     // Update is called once per frame
@@ -27,14 +32,58 @@ public class LevelUpButtonMngr : MonoBehaviour
         
     }
 
-    public void Option1(){
-        print(option1Image.sprite.ToString());
-        if(option1Image.sprite.ToString() == "chi_blast_1 (UnityEngine.Sprite)"){
-            print("found goku");
+    public void Option1 () {
+        setAbilities(option1Image.sprite.name);
+    }
+    public void Option2 () {
+        setAbilities(option2Image.sprite.name);
+    }
+    public void Option3 () {
+        setAbilities(option3Image.sprite.name);
+    }
+
+    private void setAbilities (string prompt) {
+        if(prompt == "strength"){
+            //increase damage
+            print("more Strength");
         }
 
-        bullet.force = 10f;
+        if (prompt == "multi"){
+            //add bullets
+            print("added bullets");
+        }
+        if (prompt == "speed") {
+            //increase projectile speed
+            bullet.force = 10f;
+        }
+
         PowerUpCanvas.SetActive(false);
         Time.timeScale = 1f;
+    }
+
+    public void setImages () {
+        Shuffle<Sprite>(testImage);
+
+        option1Image.sprite = testImage[0];
+        option2Image.sprite = testImage[1];
+        option3Image.sprite = testImage[2];
+    }
+
+
+    public static void Shuffle<Sprite>(Sprite[] array)
+    {
+        System.Random rng = new System.Random();
+
+        // Loop through the array from the end to the beginning
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            // Generate a random index within the range [0, i+1)
+            int randomIndex = rng.Next(i + 1);
+
+            // Swap the elements at randomIndex and i
+            Sprite temp = array[i];
+            array[i] = array[randomIndex];
+            array[randomIndex] = temp;
+        }
     }
 }
