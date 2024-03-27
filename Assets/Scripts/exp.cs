e46da97be9b8151dfcdeeb1c6ab9f3a5ef142783
Afnan Khalid <afnank19@gmail.com>
2024-03-27 22:02:06 +0500

    add glow to exp, add velocity to exp

--- Assets/Scripts/exp.cs
+++ Assets/Scripts/exp.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class exp : MonoBehaviour
 {
+    [SerializeField]
+    Rigidbody2D exp_rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        int x = Random.Range(-1,2);
+        int y = Random.Range(-1,2);
+
+        Vector2 spawn_velocity = new Vector2(x,y);
+
+        exp_rb.velocity = spawn_velocity;
     }
 
     void OnTriggerEnter2D(Collider2D coll){
